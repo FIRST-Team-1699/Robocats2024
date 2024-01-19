1436da94bbf0f3050936dcb42f6aaa07894c9039
Kevin <96791292+alex-1699@users.noreply.github.com>
2024-01-18 19:48:12 -0500

    updated constants

--- src/main/java/frc/team1699/Constants.java
+++ src/main/java/frc/team1699/Constants.java
@@ -2,4 +2,10 @@ package frc.team1699;
 public class Constants {
     // The DI/O port where the LED strip is plugged in
     public static final int kLEDPort = 0;
+    public static class SwerveConstants {
+        public static final double kDeadband = .2;
+        public static final double kMaxspeed = Units.feetToMeters(15,1);
+        public static final double kMaxRetationalSpeed = Units.feetToMeters(10); // DOES THIS NEED TO BE DIFFRENT THAN MAX SPEED? 
+    }
 }
+public stat
