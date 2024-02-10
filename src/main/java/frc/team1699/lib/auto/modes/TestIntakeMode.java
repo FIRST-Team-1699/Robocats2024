bff430883484e12b70d3ef96724bbdcb439e3072
Kevin <2007kdogrules@gmail.com>
2024-02-10 11:28:55 -0500

    created intake auto mode

--- src/main/java/frc/team1699/lib/auto/modes/TestIntakeMode.java
+++ src/main/java/frc/team1699/lib/auto/modes/TestIntakeMode.java
@@ -1,25 +1,31 @@
 package frc.team1699.lib.auto.modes;
 
 import java.util.ArrayList;
+
 import com.pathplanner.lib.path.PathPlannerTrajectory;
+
 import frc.team1699.lib.auto.events.Event;
 import frc.team1699.lib.auto.events.FollowTrajectoryEvent;
+import frc.team1699.lib.auto.events.RunIntakeEvent;
+import frc.team1699.lib.auto.events.StopIntakeEvent;
 import frc.team1699.subsystems.Drive;
+import frc.team1699.subsystems.Intake;
 
-public class TestTrajectoryMode extends AutoMode {
+public class TestIntakeMode extends AutoMode {
     private ArrayList<Event> events;
     private int i;
 
-    public TestTrajectoryMode(PathPlannerTrajectory trajectory, Drive swerve) {
-        events = new ArrayList<Event>();
+    public TestIntakeMode(PathPlannerTrajectory trajectory, Intake intake, Drive swerve) {
+        events.add(new RunIntakeEvent(intake));
         events.add(new FollowTrajectoryEvent(trajectory, swerve));
+        events.add(new StopIntakeEvent(intake));
 
         i = 0;
     }
 
     @Override
     public void initialize() {
-        events.get(i).initialize();
+        events.get(i).initialize();    
     }
 
     @Override
