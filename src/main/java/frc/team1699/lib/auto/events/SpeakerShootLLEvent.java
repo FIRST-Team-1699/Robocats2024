a3acd6e3e8b8b25f641b48b854a19e93e13c0c5f
PaarkG <2007kdogrules@gmail.com>
2024-03-19 18:31:22 -0400

    timeout to prevent waiting in auto if note is missed

--- src/main/java/frc/team1699/lib/auto/events/SpeakerShootLLEvent.java
+++ src/main/java/frc/team1699/lib/auto/events/SpeakerShootLLEvent.java
@@ -1,24 +1,18 @@
 package frc.team1699.lib.auto.events;
 
-import edu.wpi.first.wpilibj.Timer;
 import frc.team1699.subsystems.Manipulator;
 import frc.team1699.subsystems.Manipulator.ManipulatorStates;
 
 public class SpeakerShootLLEvent extends Event {
     private Manipulator manipulator;
-    private Timer timer;
 
     public SpeakerShootLLEvent(Manipulator manipulator) {
         this.manipulator = manipulator;
-        this.timer = new Timer();
-        timer.stop();
-        timer.reset();
     }
 
     @Override
     public void initialize() {
         manipulator.setWantedState(ManipulatorStates.SPEAKER_LL_SHOOT);
-        timer.start();
     }
 
     @Override
